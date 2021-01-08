956d36a8f888c568fd83a7506af43f79f674a7f9
Hemant <hemantcodes@gmail.com>
2021-01-08 16:19:49 +0530

    CPWRec

--- testpro.cpp
+++ testpro.cpp
@@ -1,28 +1,29 @@
 #include <stdio.h>
 #include <math.h>
-int main(){
-float x, y, sum;
-char ch;
-print:
-    printf ("Enter the first number:");
-    scanf ("%f",&x);
-    printf ("Enter the second number:");
-    scanf ("%f",&y);
-    sum=x+y;
-    printf ("\nThe total number is:%.2f\n",sum);
-again:
-    printf ("\n\tDo you want to repeat the operation(Y/N): ");
-    scanf (" %c", &ch);
+int main()
+{
+    float x, y, sum;
+    char ch;
+    print:
+        printf ("Enter the first number:");
+        scanf ("%f",&x);
+        printf ("Enter the second number:");
+        scanf ("%f",&y);
+        sum=x+y;
+        printf ("\nThe total number is:%.2f\n",sum);
+    again:
+        printf ("\n\tDo you want to repeat the operation(Y/N): ");
+        scanf (" %c", &ch);
 
-    if(ch == 'y' || ch == 'Y'){
-        goto print;
-    }
-    else if(ch == 'n' || ch == 'N'){
-        return 0;
-    }
-    else{
-        printf("\n\tPlease enter Yes or NO.\n");
-        goto again;
-    }
-   return 0;
+        if(ch == 'y' || ch == 'Y'){
+            goto print;
+        }
+        else if(ch == 'n' || ch == 'N'){
+            return 0;
+        }
+        else{
+            printf("\n\tPlease enter Yes or NO.\n");
+            goto again;
+        }
+    return 0;
 }
